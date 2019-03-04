600c641b6a1e2f3892f2cc7dd5d5c0044c05f6c6
Emir Bosques <emirbosques@gmail.com>
2019-03-04 23:13:20 +0100

    Retrieving profile data from google profile

--- src/app/components/protegida/protegida.component.ts
+++ src/app/components/protegida/protegida.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../service/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./protegida.component.css']
 })
 export class ProtegidaComponent implements OnInit {
+  profile: any = {};
+  loading = true;
 
-  constructor() { }
+  constructor( public auth: AuthService) { 
+    
+  }
 
   ngOnInit() {
+
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+      console.log('profile 0', this.profile);
+      setTimeout(() => {
+        this.loading = false;
+      }, 6000);
+      
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+        console.log('profile 1 ', this.profile);
+        setTimeout(() => {
+          this.loading = false;
+        }, 6000);
+      });
+    }
+
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,8 +16,10 @@ export class AuthService {
     domain: 'kratos-srv.eu.auth0.com',
     responseType: 'token id_token',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
+
+  userProfile: any;
 
   constructor(public router: Router) {
     this._idToken = '';
@@ -88,5 +90,21 @@ export class AuthService {
   public login(): void {
     this.auth0.authorize();
   }
+
+
+  public getProfile(cb): void {
+    if (!this._accessToken) {
+      throw new Error('Access Token must exist to fetch profile');
+    }
+  
+    const self = this;
+    this.auth0.client.userInfo(this._accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+  }
+
 
 }
